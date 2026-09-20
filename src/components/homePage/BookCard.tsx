fea18870b2bca33b2daa9794bde809684d60509f
Md Sabbir Hossain <hossainsabbir309@gmail.com>
2026-09-20 23:59:49 +0600

    added some in book card

--- src/components/homePage/BookCard.tsx
+++ src/components/homePage/BookCard.tsx
@@ -1,10 +1,28 @@
 import { IBook } from "@/types/bookTypes";
+import Image from "next/image";
 import React from "react";
 
 const BookCard = ({ book }: { book: IBook }) => {
   return (
-    <div>
-      <p>{book.bookName}</p>
+    <div className="border border-gray-200 rounded-md p-5">
+      <div>
+        <Image
+          src={book.image}
+          alt={`${book.bookName} image`}
+          width={500}
+          height={500}
+        ></Image>
+      </div>
+
+      <div className="flex flex-col gap-3">
+        <p>{book.bookName}</p>
+        <p>By: {book.author}</p>
+
+        <ul className="flex justify-between gap-5">
+          <li>{book.publisher}</li>
+          <li>{book.rating}</li>
+        </ul>
+      </div>
     </div>
   );
 };
